29702a7a187cf93b7e0bdab5084d31c681e316e6
JuniaMCampos <juniacamposjc@gmail.com>
2024-03-31 18:54:07 -0300

    Configuração API - Adicionado Controller Classe Usuario

---
+++ src/Api/mei/Controllers/UsuariosController.cs
@@ -0,0 +1,76 @@
+﻿using mei.Models;
+using mei.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mei.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+            private readonly UsuariosService _usuariosService;
+            public UsuariosController(UsuariosService usuariosService)
+            {
+                _usuariosService = usuariosService;
+            }
+            [HttpGet]
+            public async Task<List<Usuario>> Get() =>
+              await _usuariosService.GetAsync();
+
+            [HttpGet("{id:length(24)}")]
+            public async Task<ActionResult<Usuario>> GetById(string id)
+            {
+                var user = await _usuariosService.GetAsync(id);
+
+                if (user is null)
+                {
+                    return NotFound();
+                }
+
+                return user;
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Post(Usuario newUser)
+            {
+                await _usuariosService.CreateAsync(newUser);
+
+                return CreatedAtAction("GetById", new { id = newUser.Id }, newUser);
+            }
+            [HttpPut("{id:length(24)}")]
+            public async Task<IActionResult> Update(string id, Usuario updatedUser)
+            {
+                var user = await _usuariosService.GetAsync(id);
+
+                if (user is null)
+                {
+                    return NotFound();
+                }
+
+                updatedUser.Id = user.Id;
+
+                await _usuariosService.UpdateAsync(id, updatedUser);
+
+                return NoContent();
+            }
+
+            [HttpDelete("{id:length(24)}")]
+            public async Task<IActionResult> Delete(string id)
+            {
+                var user = await _usuariosService.GetAsync(id);
+
+                if (user is null)
+                {
+                    return NotFound();
+                }
+
+                await _usuariosService.RemoveAsync(id);
+
+                return NoContent();
+            }
+        
+    }
+
+}
+
