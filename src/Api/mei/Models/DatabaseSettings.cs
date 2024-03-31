b8e7251228e006649fce94ebde9eab8762774af8
JuniaMCampos <juniacamposjc@gmail.com>
2024-03-31 18:54:24 -0300

    Configuração API - Adicionado Model Classe Usuario

--- src/Api/mei/Models/DatabaseSettings.cs
+++ src/Api/mei/Models/DatabaseSettings.cs
@@ -5,6 +5,7 @@
         public string ConnectionString { get; set; } = null!;
         public string DatabaseName { get; set; } = null!;
         public string MeisCollectionName { get; set; } = null!;
+        public string UsuariosCollectionName { get; set; } = null!;
         //public string FaturamentosCollectionName { get; set; } = null!;
         //public string CategoriasCollectionName { get; set; } = null!;
         //public string ClientesCollectionName { get; set; } = null!;
---
+++ src/Api/mei/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace mei.Models
+{
+    public class Usuario
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
