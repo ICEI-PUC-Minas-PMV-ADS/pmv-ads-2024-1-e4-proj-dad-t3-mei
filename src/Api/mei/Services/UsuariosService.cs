d19756f8b56d37a5893d35c80482d3a5131419f1
JuniaMCampos <juniacamposjc@gmail.com>
2024-03-31 18:54:39 -0300

    Configuração API - Adicionado Service Classe Usuario

---
+++ src/Api/mei/Services/UsuariosService.cs
@@ -0,0 +1,37 @@
+﻿using mei.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace mei.Services
+{
+    public class UsuariosService
+    {
+        private readonly IMongoCollection<Usuario> _usuariosCollection;
+
+        public UsuariosService(
+            IOptions<DatabaseSettings> meiDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                meiDatabaseSettings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(
+                meiDatabaseSettings.Value.DatabaseName);
+            _usuariosCollection = mongoDatabase.GetCollection<Usuario>(
+                meiDatabaseSettings.Value.UsuariosCollectionName);
+        }
+        public async Task<List<Usuario>> GetAsync() =>
+            await _usuariosCollection.Find(_ => true).ToListAsync();
+        public async Task<Usuario?> GetAsync(string id) =>
+        await _usuariosCollection
+            .Find(x => x.Id == id)
+            .FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Usuario newUsuario) =>
+            await _usuariosCollection.InsertOneAsync(newUsuario);
+
+        public async Task UpdateAsync(string id, Usuario updatedUsuario) =>
+            await _usuariosCollection.ReplaceOneAsync(x => x.Id == id, updatedUsuario);
+
+        public async Task RemoveAsync(string id) =>
+            await _usuariosCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
