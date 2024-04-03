3e00936ff6610cda8ff9c51c9b0cf935683d4378
JuniaMCampos <juniacamposjc@gmail.com>
2024-04-02 21:49:06 -0300

    Configuração API - Adicionado Service Classe Servico

---
+++ src/Api/mei/Services/ServicosService.cs
@@ -0,0 +1,35 @@
+﻿using mei.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace mei.Services
+{
+    public class ServicosService
+    {
+        private readonly IMongoCollection<Servico> _servicosCollection;
+
+        public ServicosService(
+            IOptions<DatabaseSettings> meiDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                meiDatabaseSettings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(
+                meiDatabaseSettings.Value.DatabaseName);
+            _servicosCollection = mongoDatabase.GetCollection<Servico>(
+                meiDatabaseSettings.Value.ServicosCollectionName);
+        }
+        public async Task<List<Servico>> GetAsync() =>
+            await _servicosCollection.Find(_ => true).ToListAsync();
+        public async Task<Servico?> GetAsync(string id) =>
+        await _servicosCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Servico newServico) =>
+            await _servicosCollection.InsertOneAsync(newServico);
+
+        public async Task UpdateAsync(string id, Servico updatedServico) =>
+            await _servicosCollection.ReplaceOneAsync(x => x.Id == id, updatedServico);
+
+        public async Task RemoveAsync(string id) =>
+            await _servicosCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
