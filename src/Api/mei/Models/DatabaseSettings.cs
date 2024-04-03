a10deded025b0331299350baafd27f65f56ebb9c
JuniaMCampos <juniacamposjc@gmail.com>
2024-04-02 21:39:46 -0300

    Configuração API - Adicionado Model Classe Peoduto

--- src/Api/mei/Models/DatabaseSettings.cs
+++ src/Api/mei/Models/DatabaseSettings.cs
@@ -6,9 +6,11 @@
         public string DatabaseName { get; set; } = null!;
         public string MeisCollectionName { get; set; } = null!;
         public string UsuariosCollectionName { get; set; } = null!;
-        //public string FaturamentosCollectionName { get; set; } = null!;
+        public string FaturamentosCollectionName { get; set; } = null!;
         public string CategoriasCollectionName { get; set; } = null!;
         public string ClientesCollectionName { get; set; } = null!;
+        public string DespesasCollectionName { get; set; } = null!;
+        public string ProdutosCollectionName { get; set; } = null!;
 
     }
 }
---
+++ src/Api/mei/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace mei.Models
+{
+    public class Produto
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
