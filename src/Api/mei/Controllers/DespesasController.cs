74d56cd2592c7f8c5f8feb4ae40731536db15f97
AnnaC-Mota <rochamotaannaclara@gmail.com>
2024-04-05 20:46:45 -0300

    Configuração API - Adicionado Controller/Model/Service Classe Despesa

--- src/Api/mei/Controllers/DespesasController.cs
+++ src/Api/mei/Controllers/DespesasController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using mei.Models;
+using mei.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace mei.Controllers
@@ -7,5 +8,66 @@ namespace mei.Controllers
     [ApiController]
     public class DespesasController : ControllerBase
     {
+        private readonly DespesasService _despesasService;
+        public DespesasController(DespesasService despesasService)
+        {
+            _despesasService = despesasService;
+        }
+        [HttpGet]
+
+        public async Task<List<Despesa>> Get() =>
+          await _despesasService.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Despesa>> GetById(string id)
+        {
+            var despesa = await _despesasService.GetAsync(id);
+
+            if (despesa is null)
+            {
+                return NotFound();
+            }
+
+            return despesa;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Despesa newDespesa)
+        {
+            await _despesasService.CreateAsync(newDespesa);
+
+            return CreatedAtAction("GetById", new { id = newDespesa.Id }, newDespesa);
+        }
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Despesa updatedDespesa)
+        {
+            var despesa = await _despesasService.GetAsync(id);
+
+            if (despesa is null)
+            {
+                return NotFound();
+            }
+
+            updatedDespesa.Id = despesa.Id;
+
+            await _despesasService.UpdateAsync(id, updatedDespesa);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var despesa = await _despesasService.GetAsync(id);
+
+            if (despesa is null)
+            {
+                return NotFound();
+            }
+
+            await _despesasService.RemoveAsync(id);
+
+            return NoContent();
+        }
     }
 }
