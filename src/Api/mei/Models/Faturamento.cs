f2abfb92fb90b3e632a01479a775ae2786bd0b35
Geraldo Homero <geraldohomero@protonmail.com>
2024-04-06 16:53:21 -0300

    Cria Faturamento.cs - API

---
+++ src/Api/mei/Models/Faturamento.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace mei.Models
+{
+    public class Faturamento
+    { 
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public List<string>? ProdutosId { get; set; }
+        public List<string>? ServicosId { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataFaturamento { get; set; }
+        public MeioDePagamento MeioDePagamento { get; set; }
+    }
+
+    public enum MeioDePagamento
+    {
+        [BsonElement("Dinheiro")]
+        Dinheiro,
+        [BsonElement("Cartao")]
+        Cartao,
+        [BsonElement("Pix")]
+        Pix,
+        [BsonElement("Outro")]
+        Outro
+
+    }
+}
