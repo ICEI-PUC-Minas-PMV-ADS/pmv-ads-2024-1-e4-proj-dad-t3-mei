190d2114fbe34b63ec611ef8cda72c2672429f7b
Junia Campos <113808083+JuniaMCampos@users.noreply.github.com>
2024-05-05 22:34:52 -0300

    Add WebFront - Telas deProcurar Contador e Registrar Categoria

---
+++ src/WebFront/src/components/ProcurarContadores/ProcurarContadores.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+
+const ProcurarContadores = () => {
+  return <></>;
+};
+
+export default ProcurarContadores;
