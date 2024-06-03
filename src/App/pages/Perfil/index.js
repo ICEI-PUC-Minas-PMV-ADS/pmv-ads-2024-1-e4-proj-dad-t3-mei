29aed54fbbb5d93a3b0f551de49cf44de63630ab
CaroolMozelli <114624183+CaroolMozelli@users.noreply.github.com>
2024-06-02 21:12:47 -0300

    Front APP - Page Perfil/Registros

---
+++ src/App/pages/Perfil/index.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Alert, ActivityIndicator } from 'react-native';
+import { Button, Card } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { jwtDecode } from 'jwt-decode';
+import axios from 'axios';
+import Dialog from "react-native-dialog";
+import { useNavigation } from '@react-navigation/native';
+
+import API_URLS from '../../config/apiUrls';
+
+function Perfil() {
+  const [usuario, setUsuario] = useState(null);
+  const [showId, setShowId] = useState(false);
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const [fieldToEdit, setFieldToEdit] = useState("");
+  const [newValue, setNewValue] = useState("");
+  const [token, setToken] = useState(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+        setToken(token);
+        const decodedToken = jwtDecode(token);
+        const userId = decodedToken && decodedToken.nameid;
+
+
+        console.log('userId:', userId);
+
+        if (userId) {
+          axios.get(`${API_URLS.USUARIOS}/${userId}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+            .then((response) => {
+              setUsuario(response.data);
+            })
+            .catch((error) => {
+              console.error('Erro ao recuperar os dados do usuário:', error);
+            });
+        } else {
+          console.error('Erro: userId está indefinido');
+        }
+      } catch (error) {
+        console.error('Erro ao decodificar o token:', error);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  const handleEdit = (field) => {
+    setFieldToEdit(field);
+    setNewValue(''); // Limpa o estado newValue
+    setDialogVisible(true);
+  };
+
+  const handleCancel = () => {
+    setDialogVisible(false);
+  };
+
+
+  const handleConfirm = () => {
+    const updatedUser = { ...usuario, [fieldToEdit.toLowerCase()]: newValue };
+    axios.put(`${API_URLS.USUARIOS}/${usuario.id}`, updatedUser, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status === 200 || response.status === 204) {
+          setUsuario(updatedUser);
+        } else {
+          throw new Error(`Erro ao atualizar o usuário: ${response.status} ${response.statusText}`);
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao atualizar o usuário:", error);
+      });
+    setDialogVisible(false);
+  };
+
+  const handleLogoff = async () => {
+    await AsyncStorage.removeItem('token');
+    setToken(null);
+    setUsuario(null);
+    navigation.navigate('Home');
+    // Redefine o histórico de navegação após o Logoff
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Home' }],
+    });
+  };
+
+  if (!usuario) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ flex: 1, padding: 10, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 15, width: '100%' }}>
+        <Text><Text style={{ fontWeight: 'bold' }}>ID:</Text> {showId && <Text>{usuario.id}</Text>}</Text>
+        <Button mode="contained" onPress={() => setShowId(!showId)}>{showId ? 'Esconder' : 'Mostrar'}</Button>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 15, width: '100%' }}>
+        <Text><Text style={{ fontWeight: 'bold' }}>Nome:</Text> {usuario.nome}</Text>
+        <Button mode="contained" onPress={() => handleEdit('Nome')}>Editar</Button>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 15, width: '100%' }}>
+        <Text><Text style={{ fontWeight: 'bold' }}>Email:</Text> {usuario.email}</Text>
+        <Button mode="contained" onPress={() => handleEdit('Email')}>Editar</Button>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 15, width: '100%' }}>
+        <Text><Text style={{ fontWeight: 'bold' }}>CNPJ:</Text> {usuario.cnpj}</Text>
+        <Button mode="contained" onPress={() => handleEdit('CNPJ')}>Editar</Button>
+      </View>
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <Button mode="contained" color="red" onPress={handleLogoff}>Sair</Button>
+        <Dialog.Container visible={dialogVisible}>
+          <Dialog.Title>Editar {fieldToEdit}</Dialog.Title>
+          <Dialog.Input onChangeText={setNewValue} value={newValue} />
+          <Dialog.Button label="Cancelar" onPress={handleCancel} />
+          <Dialog.Button label="OK" onPress={handleConfirm} />
+        </Dialog.Container>
+      </View>
+    </View>
+
+  );
+}
+
+export default Perfil;
---
+++ src/App/pages/Registros/index.js
@@ -0,0 +1,209 @@
+import React, { useState, useEffect } from 'react';
+import { ScrollView, View, Text, StyleSheet } from 'react-native';
+import { Table, Row } from 'react-native-table-component';
+import { IconButton } from 'react-native-paper';
+import API_URLS from '../../config/apiUrls';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { jwtDecode } from 'jwt-decode';
+
+const Registros = () => {
+  const [faturamentos, setFaturamentos] = useState([]);
+  const [despesas, setDespesas] = useState([]);
+  const [produtos, setProdutos] = useState([]);
+  const [servicos, setServicos] = useState([]);
+  const [categorias, setCategorias] = useState([]);
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const fetchTokenAndDecode = async () => {
+      const token = await AsyncStorage.getItem("token");
+      if (token) {
+        const decodedToken = jwtDecode(token);
+        setUserId(decodedToken.nameid);
+      }
+    };
+
+    fetchTokenAndDecode();
+  }, []);
+
+  useEffect(() => {
+    if (userId) {
+      Promise.all([
+        fetch(`${API_URLS.FATURAMENTOS}`).then((response) => response.json()),
+        fetch(`${API_URLS.DESPESAS}`).then((response) => response.json()),
+        fetch(`${API_URLS.PRODUTOS}`).then((response) => response.json()),
+        fetch(`${API_URLS.SERVICOS}`).then((response) => response.json()),
+        fetch(`${API_URLS.CATEGORIAS}`).then((response) => response.json())
+      ])
+        .then(
+          ([faturamentosData, despesasData, produtosData, servicosData, categoriasData]) => {
+            setFaturamentos(
+              faturamentosData.filter((fat) => fat.usuarioId === userId)
+            );
+            setDespesas(despesasData.filter((des) => des.usuarioId === userId));
+            setProdutos(produtosData.filter((pro) => pro.usuarioId === userId));
+            setServicos(servicosData.filter((ser) => ser.usuarioId === userId));
+            setCategorias(categoriasData.filter((cat) => cat.usuarioId === userId));
+          }
+        )
+        .catch((error) => {
+          console.error("Erro ao buscar dados:", error);
+        });
+    }
+  }, [userId]);
+
+  const handleExcluirRegistro = async (tipo, id) => {
+    try {
+      let url;
+      switch (tipo) {
+        case "Faturamentos":
+          url = `${API_URLS.FATURAMENTOS}/${id}`;
+          break;
+        case "Despesas":
+          url = `${API_URLS.DESPESAS}/${id}`;
+          break;
+        case "Produtos":
+          url = `${API_URLS.PRODUTOS}/${id}`;
+          break;
+        case "Servicos":
+          url = `${API_URLS.SERVICOS}/${id}`;
+          break;
+        case "Categorias":
+          url = `${API_URLS.CATEGORIAS}/${id}`;
+          break;
+        default:
+          throw new Error(`Tipo desconhecido: ${tipo}`);
+      }
+
+      const response = await fetch(url, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        throw new Error(`Erro ao excluir o ${tipo}`);
+      }
+
+      // Restante do código...
+    } catch (error) {
+      console.error(`Erro ao excluir o ${tipo}: `, error);
+    }
+  };
+
+  const getNomeById = (id, array) => {
+    const item = array.find(item => item.id === id);
+    return item ? item.nome : '';
+  };
+
+  const renderFaturamentosRow = (fat) => {
+    return {
+      data: fat.dataFaturamento,
+      venda: fat.nome,
+      produto: getNomeById(fat.produtoId, produtos),
+      servico: getNomeById(fat.servicoId, servicos),
+      meioDePagamento: fat.meioDePagamento,
+      valor: fat.valor.toString(),
+      button: <IconButton
+        icon="delete"
+        color="red"
+        size={20}
+        onPress={() => handleExcluirRegistro("Faturamentos", fat.id)}
+      />
+    };
+  };
+
+  const renderDespesasRow = (des) => {
+    return {
+      dataDespesa: des.dataDespesa,
+      nome: des.nome,
+      categoria: getNomeById(des.categoriaId, categorias),
+      valor: des.valor.toString(),
+      button: <IconButton
+        icon="delete"
+        color="red"
+        size={20}
+        onPress={() => handleExcluirRegistro("Despesas", des.id)}
+      />
+    };
+  };
+
+  const renderTable = (head, data, renderRow, title) => {
+    const widthArr = new Array(head.length).fill(80); // Altere 80 para a largura desejada
+
+    return (
+      <ScrollView horizontal={true}>
+        <View>
+          <Text>{title}</Text>
+          <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+            <Row data={head} style={styles.head} widthArr={widthArr} />
+            {data.map((rowData, index) => (
+              <Row key={index} data={Object.values(renderRow(rowData))} widthArr={widthArr} />
+            ))}
+          </Table>
+        </View>
+      </ScrollView>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        {renderTable(
+          ["Data", "Venda", "Produto", "Serviço", "Meio de Pagamento", "Valor", ""],
+          faturamentos,
+          renderFaturamentosRow,
+          "Registros de vendas"
+        )}
+        {renderTable(
+          ["Data", "Despesa", "Categoria", "Valor", ""],
+          despesas,
+          renderDespesasRow,
+          "Registros de despesas"
+        )}
+      </View>
+    </View>
+  );
+
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        {renderFaturamentosTable(
+          ["Data", "Venda", "Produto", "Serviço", "Meio de Pagamento", "Valor", ""],
+          faturamentos,
+          renderFaturamentosRow,
+          "Registros de vendas"
+        )}
+        {renderTable(
+          ["Data", "Despesa", "Categoria", "Valor", ""],
+          despesas,
+          renderDespesasRow,
+          "Registros de despesas"
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    paddingTop: 30,
+    backgroundColor: '#fff'
+  },
+  box: {
+    flex: 1,
+    margin: 16,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: '#ffc107',
+    borderRadius: 8
+  },
+  head: {
+    height: 40,
+    backgroundColor: '#f1f8ff'
+  },
+});
+
+export default Registros;
